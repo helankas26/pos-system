47155426e6acab30bb6e20d923d1716be89ad0d6
Helanka Singhapurage <helankas26@gmail.com>
2023-10-29 10:15:22 +0530

    add AppInitializer

---
+++ src/main/java/com/devstack/pos/AppInitializer.java
@@ -0,0 +1,28 @@
+package main.java.com.devstack.pos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL resource = getClass().getResource("view/LoginForm.fxml");
+        Parent parent = FXMLLoader.load(resource);
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("POS");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
