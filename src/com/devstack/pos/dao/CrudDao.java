8b15a3e6ab385e587d207a20dce28a668b479d23
Helanka Singhapurage <helankas26@gmail.com>
2023-10-29 14:31:54 +0530

    add SuperDao and CrudDao

---
+++ src/com/devstack/pos/dao/CrudDao.java
@@ -0,0 +1,11 @@
+package com.devstack.pos.dao;
+
+import java.util.List;
+
+public interface CrudDao<T, ID> {
+    boolean create(T t);
+    T find(ID id);
+    boolean remove(ID id);
+    boolean modify(T t);
+    List<T> loadAll();
+}
