e9faacd2c7915e8f91e4e9e1d04e1b089c5558dd
Helanka Singhapurage <helankas26@gmail.com>
2023-11-04 23:26:25 +0530

    add adminPortal form

--- src/com/devstack/pos/controller/LoginFormController.java
+++ src/com/devstack/pos/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package com.devstack.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginFormController {
+
+    public AnchorPane loginContext;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) loginContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/AdminPortal.fxml"))));
+        stage.centerOnScreen();
+    }
 }
