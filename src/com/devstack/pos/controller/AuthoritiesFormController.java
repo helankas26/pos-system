125de30302acc8e27d2900da674468bec3089254
Helanka Singhapurage <helankas26@gmail.com>
2023-11-11 14:32:21 +0530

    add AuthorityDetails form

--- src/com/devstack/pos/controller/AuthoritiesFormController.java
+++ src/com/devstack/pos/controller/AuthoritiesFormController.java
@@ -1,15 +1,63 @@
 package com.devstack.pos.controller;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class AuthoritiesFormController {
     public AnchorPane authoritiesContext;
+    public AnchorPane overlayPane;
+    public AnchorPane overlayPaneContext;
+    public TableView tblAuthorities;
+
+    public void initialize() {
+        overlayPane.setVisible(false);
+        overlayPane.setDisable(true);
+
+        overlayPane.setOnMouseClicked(event -> {
+            FadeTransition popup = new FadeTransition(Duration.seconds(0.4), overlayPane);
+            popup.setFromValue(1.0);
+            popup.setToValue(0);
+            popup.play();
+
+            popup.setOnFinished(innerEvent -> {
+                overlayPane.setVisible(false);
+                overlayPane.setDisable(true);
+                overlayPaneContext.getChildren().clear();
+            });
+        });
+    }
+
+    public void tableOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        tblAuthorities.getSelectionModel().getSelectedItem();
+        openAuthorityDetailsModal("code is here");
+    }
+
+    public void openAuthorityDetailsModal(String code) throws IOException {
+        overlayPane.setVisible(true);
+        overlayPane.setDisable(false);
+        FadeTransition popup = new FadeTransition(Duration.seconds(0.4), overlayPane);
+        popup.setFromValue(0);
+        popup.setToValue(1.0);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/AuthorityDetailsForm.fxml"));
+        AnchorPane AuthorityDetailsForm = loader.load();
+        AuthorityDetailsFormController controller = loader.getController();
+        controller.initialize(code);
+
+        overlayPaneContext.getChildren().clear();
+        overlayPaneContext.getChildren().add(AuthorityDetailsForm);
+
+        popup.play();
+    }
 
     private void setUi(String location) throws IOException {
         Stage stage = (Stage) authoritiesContext.getScene().getWindow();
---
+++ src/com/devstack/pos/controller/AuthorityDetailsFormController.java
@@ -0,0 +1,11 @@
+package com.devstack.pos.controller;
+
+import javafx.scene.layout.AnchorPane;
+
+public class AuthorityDetailsFormController {
+    public AnchorPane authorityDetailsContext;
+
+    public void initialize(String code) {
+        System.out.println(code);
+    }
+}
