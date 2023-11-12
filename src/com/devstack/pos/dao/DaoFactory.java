6fb318a2700ae973e9dcf2eca447cc569222729e
Helanka Singhapurage <helankas26@gmail.com>
2023-11-12 21:08:26 +0530

    add AccessPoint DAO

--- src/com/devstack/pos/dao/DaoFactory.java
+++ src/com/devstack/pos/dao/DaoFactory.java
@@ -1,20 +1,25 @@
 package com.devstack.pos.dao;
 
+import com.devstack.pos.dao.custom.impl.AccessPointDaoImpl;
 import com.devstack.pos.dao.custom.impl.UserDaoImpl;
 import com.devstack.pos.dao.custom.impl.UserRoleDaoImpl;
 
 public class DaoFactory {
-    private DaoFactory(){}
-    public enum DaoType{
-        USER,
-        USER_ROLE
+    private DaoFactory() {
     }
-    public static <T> T getDao(DaoType type){
-        switch (type){
+
+    public enum DaoType {
+        USER, USER_ROLE, ACCESS_POINT
+    }
+
+    public static <T> T getDao(DaoType type) {
+        switch (type) {
             case USER:
                 return (T) new UserDaoImpl();
             case USER_ROLE:
                 return (T) new UserRoleDaoImpl();
+            case ACCESS_POINT:
+                return (T) new AccessPointDaoImpl();
             default:
                 return null;
         }
